5aa023556a0c2a3371c438d78488041297b927c4
Angel Barrera Sanchez <angelbarrerasanchez@protonmail.com>
2020-08-09 11:58:45 +0200

    kubectl plugin code refactored

--- kubectl-k8spin.py
+++ kubectl-k8spin.py
@@ -4,6 +4,28 @@ import argparse
 import json
 import subprocess
 
+ORG = {
+    "Name": ("metadata", "name"),
+    "CPU": ("spec", "resources", "cpu"),
+    "Memory": ("spec", "resources", "memory")
+}
+
+
+TENANT = {
+    "Organization": ("metadata", "labels", "k8spin.cloud/org"),
+    "Name": ("metadata", "name"),
+    "CPU": ("spec", "resources", "cpu"),
+    "Memory": ("spec", "resources", "memory")
+}
+
+SPACE = {
+    "Organization": ("metadata", "labels", "k8spin.cloud/org"),
+    "Tenant": ("metadata", "labels", "k8spin.cloud/tenant"),
+    "Name": ("metadata", "name"),
+    "CPU": ("spec", "resources", "cpu"),
+    "Memory": ("spec", "resources", "memory")
+}
+
 
 class CommandArguments():
     def __init__(self, org=None, tenant=None, space=None, namespace=None):
@@ -13,30 +35,53 @@ class CommandArguments():
         self.namespace = namespace
 
 
+def extract_from_dictionary(dictionary, *keys_or_indexes):
+    value = dictionary
+    for key_or_index in keys_or_indexes:
+        value = value[key_or_index]
+    return value
+
+
+def tab_print(values):
+    line = "{:<20s}"*len(values)
+    print(line.format(*values))
+
+
+def print_item(item, kind):
+    tab_print([extract_from_dictionary(item, *kind[key])
+               for key in kind.keys()])
+
+
+def execute_get_command(command, kind):
+    result = subprocess.run(
+        filter(None, command.split(" ")), capture_output=True)
+    if result.returncode == 0:
+        j = json.loads(result.stdout)
+        tab_print(list(kind.keys()))
+        if j.get("kind") == "List":
+            for item in j.get("items"):
+                print_item(item, kind)
+        else:
+            print_item(j, kind)
+    else:
+        print(result.stderr.decode("utf-8"))
+
+
+def execute_command(command):
+    result = subprocess.run(
+        filter(None, command.split(" ")), capture_output=True)
+    if result.returncode == 0:
+        print(result.stdout.decode("utf-8"))
+    else:
+        print(result.stderr.decode("utf-8"))
+
+
 def get_org(arg: CommandArguments):
     org = arg.org
     if not org:
         org = ""
     command = f"kubectl get org {org} -o json"
-    result = subprocess.run(
-        filter(None, command.split(" ")), capture_output=True)
-    if result.returncode == 0:
-        j = json.loads(result.stdout)
-        print("{:<20s} {:<20s} {:<20s}".format("Name", "CPU", "Memory"))
-        if j.get("items", None):
-            for item in j.get("items"):
-                name = item.get("metadata").get("name")
-                cpu = item.get("spec").get("resources").get("cpu")
-                mem = item.get("spec").get("resources").get("memory")
-                print("{:<20s} {:<20s} {:<20s}".format(name, cpu, mem))
-        else:
-            item = j
-            name = item.get("metadata").get("name")
-            cpu = item.get("spec").get("resources").get("cpu")
-            mem = item.get("spec").get("resources").get("memory")
-            print("{:<20s} {:<20s} {:<20s}".format(name, cpu, mem))
-    else:
-        print(result.stderr.decode("utf-8"))
+    execute_get_command(command, ORG)
 
 
 def delete_org(arg: CommandArguments):
@@ -44,12 +89,7 @@ def delete_org(arg: CommandArguments):
     if not org:
         org = ""
     command = f"kubectl delete org {org}"
-    result = subprocess.run(
-        filter(None, command.split(" ")), capture_output=True)
-    if result.returncode == 0:
-        print(result.stdout.decode("utf-8"))
-    else:
-        print(result.stderr.decode("utf-8"))
+    execute_command(command)
 
 
 def get_tenant(arg: CommandArguments):
@@ -61,30 +101,7 @@ def get_tenant(arg: CommandArguments):
 
     tenant = arg.tenant
     command = f"kubectl get tenant {tenant} -o json {opts}"
-    result = subprocess.run(
-        filter(None, command.split(" ")), capture_output=True)
-    if result.returncode == 0:
-        j = json.loads(result.stdout)
-        print("{:<20s} {:<20s} {:<20s} {:<20s}".format(
-            "Org", "Name", "CPU", "Memory"))
-        if tenant:
-            item = j
-            org = item.get("metadata").get("labels").get("k8spin.cloud/org")
-            name = item.get("metadata").get("name")
-            cpu = item.get("spec").get("resources").get("cpu")
-            mem = item.get("spec").get("resources").get("memory")
-            print("{:<20s} {:<20s} {:<20s} {:<20s}".format(org, name, cpu, mem))
-        else:
-            for item in j.get("items", None):
-                org = item.get("metadata").get(
-                    "labels").get("k8spin.cloud/org")
-                name = item.get("metadata").get("name")
-                cpu = item.get("spec").get("resources").get("cpu")
-                mem = item.get("spec").get("resources").get("memory")
-                print("{:<20s} {:<20s} {:<20s} {:<20s}".format(
-                    org, name, cpu, mem))
-    else:
-        print(result.stderr.decode("utf-8"))
+    execute_get_command(command, TENANT)
 
 
 def get_space(arg: CommandArguments):
@@ -97,35 +114,7 @@ def get_space(arg: CommandArguments):
     space = arg.space
 
     command = f"kubectl get space {space} -o json {opts}"
-    result = subprocess.run(
-        filter(None, command.split(" ")), capture_output=True)
-    if result.returncode == 0:
-        j = json.loads(result.stdout)
-        print("{:<20s} {:<20s} {:<20s} {:<20s} {:<20s}".format(
-            "Org", "Tenant", "Name", "CPU", "Memory"))
-        if space:
-            item = j
-            org = item.get("metadata").get("labels").get("k8spin.cloud/org")
-            tenant = item.get("metadata").get(
-                "labels").get("k8spin.cloud/tenant")
-            name = item.get("metadata").get("name")
-            cpu = item.get("spec").get("resources").get("cpu")
-            mem = item.get("spec").get("resources").get("memory")
-            print("{:<20s} {:<20s} {:<20s} {:<20s} {:<20s}".format(
-                org, tenant, name, cpu, mem))
-        else:
-            for item in j.get("items", None):
-                org = item.get("metadata").get(
-                    "labels").get("k8spin.cloud/org")
-                tenant = item.get("metadata").get(
-                    "labels").get("k8spin.cloud/tenant")
-                name = item.get("metadata").get("name")
-                cpu = item.get("spec").get("resources").get("cpu")
-                mem = item.get("spec").get("resources").get("memory")
-                print("{:<20s} {:<20s} {:<20s} {:<20s} {:<20s}".format(
-                    org, tenant, name, cpu, mem))
-    else:
-        print(result.stderr.decode("utf-8"))
+    execute_get_command(command, SPACE)
 
 
 # Parent parser
@@ -210,12 +199,12 @@ sub_command = main_args.sub_command if hasattr(
     main_args, "sub_command") else None
 
 if command and sub_command:
-    method_to_call = locals()[f"{command}_{sub_command}"]
     org = main_args.org_name if hasattr(main_args, "org_name") else None
     tenant = main_args.tenant_name if hasattr(
         main_args, "tenant_name") else None
     space = main_args.space_name if hasattr(main_args, "space_name") else None
     arg = CommandArguments(org=org, tenant=tenant, space=space)
+    method_to_call = locals()[f"{command}_{sub_command}"]
     method_to_call(arg)
 else:
     parser.print_help()
