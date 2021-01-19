23bf8e8b66d2034fe748e097000a79d07c598183
Ángel Barrera Sánchez <angel@sighup.io>
2021-01-19 20:41:26 +0100

    Add k8spin_reporter skeleton

---
+++ k8spin_reporter/k8spin_reporter/feed.py
@@ -0,0 +1,27 @@
+import pykube
+from k8spin_common import Organization, Space, Tenant
+from k8spin_common.helper import kubernetes_api
+
+
+@kubernetes_api
+def do_feed(api):
+    orgs = Organization.objects(api).all()
+    for org in orgs:
+        print(org)
+        tenants = org.tenants
+        for tenant in tenants:
+            print(tenant)
+            spaces = tenant.spaces
+            for space in spaces:
+                print(space)
+                ns = space.space_namespace
+                quota = pykube.ResourceQuota.objects(
+                    api, ns.name).get(name="quotas")
+                quota_cpu_hard = quota.obj["status"]["hard"]["requests.cpu"]
+                quota_mem_hard = quota.obj["status"]["hard"]["requests.memory"]
+                quota_cpu_used = quota.obj["status"]["used"]["requests.cpu"]
+                quota_mem_used = quota.obj["status"]["used"]["requests.memory"]
+                print(
+                    f"Organization: {org.name}. Tenant {tenant.name}. Space {space.name}")
+                print(
+                    f"CPU: {quota_cpu_used}/{quota_cpu_hard}. Memory: {quota_mem_used}/{quota_mem_hard}")
