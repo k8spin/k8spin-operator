de69475018a8e5a91ea1c020ced8d36c93e0aef0
Ángel Barrera Sánchez <angel@sighup.io>
2021-01-23 20:15:38 +0100

    Add feed database plus example of querying the DB via API

--- k8spin_reporter/app.py
+++ k8spin_reporter/app.py
@@ -1,17 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_apscheduler import APScheduler
 from healthcheck import HealthCheck
-from k8spin_reporter import feed
+
+from k8spin_reporter import data, feed
 
 scheduler = APScheduler()
+
+app = Flask(__name__)
+
 
 # interval examples
 @scheduler.task("interval", id="feed", seconds=3, misfire_grace_time=900)
 def asyncFeed():
     feed.do_feed()
 
+
+@app.route('/organization')
+def organizations():
+    orgs = data.orgs()
+    return jsonify(orgs)
+
+
 if __name__ == "__main__":
-    app = Flask(__name__)
     health = HealthCheck(app, "/health")
     scheduler.init_app(app)
     scheduler.start()
