8d3acca1cfe09b4e36be970a16d1ab800c848f04
Pau Rosello <paurosello@gmail.com>
2021-02-04 19:01:44 +0100

    merge

--- k8spin_reporter/k8spin_reporter/data.py
+++ k8spin_reporter/k8spin_reporter/data.py
@@ -11,3 +11,28 @@ def orgs(db_engine):
             "name": r[1]
         })
     return data
+
+
+def tenants():
+    data = []
+    query = f"SELECT id,name FROM organization"
+    rows = db.query(query)
+    for r in rows:
+        data.append({
+            "id": r[0],
+            "name": r[1]
+        })
+    return data
+
+
+
+def spaces():
+    data = []
+    query = f"SELECT id,name FROM organization"
+    rows = db.query(query)
+    for r in rows:
+        data.append({
+            "id": r[0],
+            "name": r[1]
+        })
+    return data
