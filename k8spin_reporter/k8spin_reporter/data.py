bbb1ca92515acaa42255ff9d0ffd346838e81934
Angel Barrera Sanchez <angelbarrerasanchez@protonmail.com>
2021-02-07 17:44:28 +0100

    Add first graph to debug.html page

--- k8spin_reporter/k8spin_reporter/data.py
+++ k8spin_reporter/k8spin_reporter/data.py
@@ -57,6 +57,52 @@ def org_current_resources(db_engine, organization_id):
     return data[0]
 
 
+# TODO: Currently it returns up to 7 days of data. Aggregated by day.
+# TODO: Make it configurable by day, hour...
+def org_history_resouces(db_engine, organization_id):
+    data = []
+    query = f"""
+        SELECT used.day, allocated.allocated_cpu, used.used_cpu, allocated.allocated_memory, used.used_memory
+
+        FROM (
+
+            SELECT organization_id, day, sum(cpu) as used_cpu, sum(memory) as used_memory
+            FROM (
+                SELECT organization_id, tenant_id, space_id, strftime('%Y%m%d', reported) as day, avg(cpu) as cpu, avg(memory) as memory
+                FROM space_usage
+                WHERE organization_id = "{organization_id}"
+                GROUP BY organization_id, tenant_id, space_id, strftime('%Y%m%d', reported)
+            )
+            GROUP BY organization_id, day
+
+        ) used,
+
+        (
+            SELECT organization_id, strftime('%Y%m%d', reported) as day, avg(cpu) as allocated_cpu, avg(memory) as allocated_memory
+            FROM organization_resources
+            WHERE organization_id = "{organization_id}"
+            GROUP BY organization_id, strftime('%Y%m%d', reported)
+
+        ) allocated
+
+        WHERE used.organization_id = "{organization_id}"
+        AND used.organization_id = allocated.organization_id
+        AND used.day = allocated.day
+        ORDER BY used.day DESC
+        LIMIT 7;
+    """
+    rows = db.query(db_engine, query, False)
+    for r in rows:
+        data.append({
+            "day": r[0],
+            "allocated_cpu": r[1],
+            "used_cpu": r[2],
+            "allocated_memory": r[3],
+            "used_memory": r[4],
+        })
+    return data
+
+
 def tenants(db_engine, organization_id):
     data = []
     query = f"""
--- k8spin_reporter/static/debug.js
+++ k8spin_reporter/static/debug.js
@@ -20,19 +20,21 @@ function organizations() {
                         <div class=\"column is-one-fifth\">\
                             "+ org.name + "\
                         </div>\
-                        <div class=\"column\" id=\""+ org.id + "\">\
+                        <div class=\"column\" id=\""+ org.id + "-resources\">\
+                        </div>\
+                        <div class=\"column\" id=\""+ org.id + "-history\">\
                         </div>\
                     </div>\
                 </div>";
             $("#orgs").append(new_box);
             organization_resources(org);
+            organization_history(org);
         }
     });
 }
 
 function organization_resources(org) {
     api_org_resources = "/api/organizations/" + org.id + "/resources";
-    console.log(api_org_resources);
     $.getJSON(api_org_resources, function (data) {
         content = "\
         <p>\
@@ -40,15 +42,81 @@ function organization_resources(org) {
             <span class=\"icon\"> \
                 <i class=\"fas fa-memory\"></i> \
             </span> \
-        <span>"+data.used_memory+"/"+data.allocated_memory+"</span> \
+        <span>"+ data.used_memory + "/" + data.allocated_memory + "</span> \
         </span></p> \
         <p>\
         <span class=\"icon-text\"> \
             <span class=\"icon\"> \
                 <i class=\"fas fa-microchip\"></i> \
             </span> \
-        <span>"+data.used_cpu+"/"+data.allocated_cpu+"</span> \
+        <span>"+ data.used_cpu + "/" + data.allocated_cpu + "</span> \
         </span></p>"
-        $("#" + org.id + "").append(content);
+        $("#" + org.id + "-resources").append(content);
+    });
+}
+
+function organization_history(org) {
+    api_org_resources = "/api/organizations/" + org.id + "/history";
+    $.getJSON(api_org_resources, function (data) {
+        // Prepare the canvas
+        content = "<canvas id=\"" + org.id + "-chart\"></canvas>";
+        $("#" + org.id + "-history").append(content);
+        // Prepare the data
+        // TODO: Consider dedup the graph. CPU separated from Memory.
+        chartData = {
+            labels: [],
+            datasets: []
+        }
+        cpu_used_records = {
+            label: "CPU Used",
+            data: []
+        }
+        memory_used_records = {
+            label: "Memory Used",
+            data: []
+        }
+        cpu_allocated_records = {
+            label: "CPU Allocated",
+            data: []
+        }
+        memory_allocated_records = {
+            label: "Memory Allocated",
+            data: []
+        }
+        for (var i = 0; i < data.length; i++) {
+            record = data[i]
+
+            chartData.labels.push(record.day);
+
+            cpu_used_records.data.push(record.used_cpu);
+            cpu_allocated_records.data.push(record.allocated_cpu);
+            memory_used_records.data.push(record.used_memory);
+            memory_allocated_records.data.push(record.allocated_memory);
+        }
+        chartData.datasets.push(cpu_used_records);
+        chartData.datasets.push(memory_used_records);
+        chartData.datasets.push(cpu_allocated_records);
+        chartData.datasets.push(memory_allocated_records);
+        // TODO Optimize
+        var chartOptions = {
+            legend: {
+                display: true,
+                position: 'top',
+                labels: {
+                    boxWidth: 80,
+                    fontColor: 'black'
+                }
+            }
+        };
+        draw_graph(chartData, chartOptions, org.id + "-chart");
+    });
+}
+
+function draw_graph(data, options, chart_id) {
+    var ctx = document.getElementById(chart_id).getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: data,
+        options: options
     });
 }
