06500899b882a58cb929cf072a8313de2042b065
Angel Barrera Sanchez <angelbarrerasanchez@protonmail.com>
2021-04-26 16:30:36 +0200

    Move validation/mutation webhook to the stable api version

--- k8spin_webhook/app.py
+++ k8spin_webhook/app.py
@@ -1,7 +1,7 @@
 import logging
 from os import getenv
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from healthcheck import HealthCheck
 from pykube.exceptions import ObjectDoesNotExist
 
@@ -21,8 +21,15 @@ app.register_blueprint(mutator.blueprint)
 
 @app.errorhandler(ObjectDoesNotExist)
 def object_does_not_exist(error):
-    logger.warning('Not found error: %s. Pass', error)
-    return jsonify({"response": {"allowed": True}})
+    request_info = request.get_json()
+    msg = f"Not found @ Kubernetes error: {error}. Passing"
+    logger.warning(msg)
+    return jsonify({"apiVersion": "admission.k8s.io/v1",
+                    "kind": "AdmissionReview",
+                    "response": {"uid": request_info["request"]["uid"],
+                                 "allowed": True,
+                                 "status": {"message": msg}
+                                 }})
 
 
 if __name__ == '__main__':
